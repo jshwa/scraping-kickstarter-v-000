6729784b117f576608d489e9f98bc71fa0d17ac5
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-09 04:54:59 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,7 @@ def create_project_hash
       image_link: project.css("div.project-thumbnail a img").attribute("src").value,
       description: project.css("p.bbcard_blurb").text,
       location: project.css("span.location-name").text,
-      precent_funded: project.css("ul.project-stats li.first.funded strong").text
+      precent_funded: 10
     }
   end
   projects
